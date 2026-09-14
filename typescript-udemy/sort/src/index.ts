f256661d1d6f7879ad20508550958cd7ee3c58a3
Randi <19175324+randimays@users.noreply.github.com>
2026-09-14 16:09:40 -0500

    Working on new sort application

---
+++ typescript-udemy/sort/src/index.ts
@@ -0,0 +1,33 @@
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    if (!this.collection || !this.collection.length) {
+      throw new Error('error');
+    }
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        let current = this.collection[j];
+        let next = this.collection[j + 1];
+
+        if (current === undefined || next === undefined) {
+          return;
+        }
+
+        if (current > next) {
+          this.collection[j] = next;
+          this.collection[j + 1] = current;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+
+sorter.sort();
+
+console.log(sorter.collection);
